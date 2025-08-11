e4692873f63c72fc42cbf181d9cce7acb42c2d7b
Tranos Taruvinga <147081112+Tranostc@users.noreply.github.com>
2025-08-11 18:29:51 +0200

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,46 +1,46 @@
 -- Create the database
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 -- Use the database
 USE alx_book_store;
 
--- Create the Authors table
+-- Create Authors table
 CREATE TABLE Authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
     author_name VARCHAR(215) NOT NULL
 );
 
--- Create the Books table
+-- Create Books table
 CREATE TABLE Books (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(130) NOT NULL,
-    author_id INT,
+    author_id INT NOT NULL,
     price DOUBLE NOT NULL,
     publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- Create the Customers table
+-- Create Customers table
 CREATE TABLE Customers (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
-    address TEXT NOT NULL
+    address TEXT
 );
 
--- Create the Orders table
+-- Create Orders table
 CREATE TABLE Orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE NOT NULL,
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT NOT NULL,
+    order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- Create the Order_Details table
+-- Create Order_Details table
 CREATE TABLE Order_Details (
-    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
+    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
