31e81201f672efc0657a58bddbb7d8e8ceb2ee8d
Tranos Taruvinga <147081112+Tranostc@users.noreply.github.com>
2025-08-12 09:12:33 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,5 @@
 --Write a script that prints the full description of the table books from the database alx_book_store in your MySQL server
 USE alx_book_store;
 SHOW CREATE TABLE Books;
+--How to run it
+mysql -u root -p alx_book_store < task_4.sql
